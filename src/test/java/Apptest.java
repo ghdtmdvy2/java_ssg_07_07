a7aa9bc30ef621fd8c35db517c48ede0022e7e1d
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-08 14:32:09 +0900

    출력을 문자열로 받기

--- src/test/java/Apptest.java
+++ src/test/java/Apptest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,5 +26,19 @@ public class Apptest {
         assertEquals("등록",cmd);
         assertEquals("명언1",content);
         assertEquals("작가1",author);
+    }
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준 출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        System.out.println("안녕");
+
+        String rs = output.toString().trim();
+
+        // 표준 출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+        assertEquals("안녕",rs);
     }
 }
