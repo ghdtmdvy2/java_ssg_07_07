b29882bffa306a4a3c2803115b69ae7f0deb3a31
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-11 14:58:02 +0900

    삭제기능 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,107 +1,82 @@
 package com.ll.exam;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
-
-    public void run() throws Exception {
-        OutputStream output = new FileOutputStream("C:\\Users\\ghdtm\\IdeaProjects\\java_ssg_07_07\\output.txt");
-        String str;
-        int check;
-        byte[] by;
+    public void run() {
         System.out.println("== 명언 SSG ==");
+
         Scanner sc = new Scanner(System.in);
-        wiseSaying ts = null;
-        wiseSaying ws = new wiseSaying();
-        List<wiseSaying> datalist = new ArrayList<>();
-        int cnt = 1;
+
+        // 가장 마지막 명언글의 번호
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+        int wiseSayingLastId = 0;
+
         outer:
         while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
-            String s[] = cmd.split("=");
-            cmd = s[0];
-            check = 0;
-            switch (cmd) {
-                case "종료":
-                    break outer;
+
+            Rq rq = new Rq(cmd);
+
+            switch (rq.getPath()) {
                 case "등록":
-                    System.out.print("명언 : ");
+                    System.out.printf("명언 : ");
                     String content = sc.nextLine().trim();
-                    System.out.print("작가 : ");
+                    System.out.printf("작가 : ");
                     String author = sc.nextLine().trim();
-                    ts = new wiseSaying(cnt,content,author);
+                    int id = ++wiseSayingLastId; // 명언 글 번호 증가
 
-                    datalist.add(ts);
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    wiseSayings.add(wiseSaying);
 
-                    System.out.println(cnt+"번 명언이 등록 되었습니다.");
-                    cnt++;
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
+                case "삭제":
+                    // URL에 입력된 id 얻기
+                    int paramId = rq.getIntParam("id", 0);
+
+                    // URL에 입력된 id가 없다면 작업중지
+                    if (paramId == 0) {
+                        System.out.println("id를 입력해주세요.");
+                        continue;
+                    }
+
+                    // URL에 입력된 id에 해당하는 명언객체 찾기
+                    WiseSaying wiseSaying__ = null;
+
+                    for (WiseSaying wiseSaying___ : wiseSayings) {
+                        if (wiseSaying___.id == paramId) {
+                            wiseSaying__ = wiseSaying___;
+                        }
+                    }
+
+                    // 찾지 못했다면 중지
+                    if (wiseSaying__ == null) {
+                        System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
+                        continue;
+                    }
+
+                    // 입력된 id에 해당하는 명언객체를 리스트에서 삭제
+                    wiseSayings.remove(wiseSaying__);
+
+                    System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
                     break;
                 case "목록":
                     System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-                    printlist(datalist);
-                    break;
-                case "삭제?id":
-                    try {
-//                        datalist.remove(Integer.parseInt(s[1]) - 1);
-
-                        for(int i = 0; i<datalist.size(); i++){
-                            int index = Integer.parseInt(s[1]);
-                            if(datalist.get(i).getId() == index){
-                                System.out.println(s[1]+"번 명언이 삭제되었습니다.");
-                                datalist.remove(i);
-                                check = 1;
-                                break;
-                            }
-
-                        }
-                        if( check == 0 ){
-                            System.out.println(s[1]+"번 명언은 존재하지 않습니다.");
-                            check = 0;
-                            break;
-                        }
-
-                    }
-                    catch(Exception e){
-                        System.out.println(s[1]+"번 명언은 존재하지 않습니다.");
+                    System.out.println("-------------------");
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
                     }
                     break;
-                case"수정=id?":
-                    for(int i = 0; i<datalist.size(); i++){
-                        int index = Integer.parseInt(s[1]);
-                        if(datalist.get(i).getId() == index){
-                            System.out.println(s[1]+"번 명언이 삭제되었습니다.");
-                            datalist.remove(i);
-                            check = 1;
-                            break;
-                        }
-
-                    }
-                    //datalist.set(1);
-                    break;
+                case "종료":
+                    break outer;
             }
         }
-        String data = ts.toString();
-        by = data.getBytes();
-        output.write(by);
-        sc.close();
-    }
 
-    private void printlist(List<wiseSaying> datalist) {
-        for(int i = datalist.size()-1; i>=0; i--){
-            System.out.println(datalist.get(i).fileString());
-        }
-    }
-    private void printjson(List<wiseSaying> datalist) {
-        for(int i = datalist.size()-1; i>=0; i--){
-            System.out.println(datalist.get(i).fileString());
-        }
+        sc.close();
     }
 }
