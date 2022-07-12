1939a3fa937370c50a60e607f3704b294a04897a
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-12 13:44:24 +0900

    Rq에 Map 도입

--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -15,26 +15,38 @@ public class Rq {
 
         queryParams = new HashMap<>();
 
-        if (urlBits.length == 2){
-            String []paramBits = urlBits[1].split("&");
-            for (String paramBit : paramBits){
-                String param[] = paramBit.split("=");
-                if(param.length == 1){
+        if (urlBits.length == 2) {
+            String queryStr = urlBits[1];
+
+            String[] paramBits = queryStr.split("&");
+
+            for (String paramBit : paramBits) {
+                String[] paramNameAndValue = paramBit.split("=", 2);
+
+                if (paramNameAndValue.length == 1) {
                     continue;
                 }
+
+                String paramName = paramNameAndValue[0].trim();
+                String paramValue = paramNameAndValue[1].trim();
+
+                queryParams.put(paramName, paramValue);
             }
         }
     }
 
     public int getIntParam(String paramName, int defaultValue) {
-       if(queryParams.containsKey(paramName) == false){
-           return defaultValue;
-       }
-       String paramValue = queryParams.get(paramName);
-       if(paramValue.length() == 0){
-           return defaultValue;
-       }
-       return Integer.parseInt(paramValue);
+        if (queryParams.containsKey(paramName) == false) {
+            return defaultValue;
+        }
+
+        String paramValue = queryParams.get(paramName);
+
+        if (paramValue.length() == 0) {
+            return defaultValue;
+        }
+
+        return Integer.parseInt(paramValue);
     }
 
     public String getPath() {
